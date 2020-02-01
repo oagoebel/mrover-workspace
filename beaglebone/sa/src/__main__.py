1129c58a558f378de1b811d4cbeb49ea223ccd66
oagoebel <oagoebel@umich.edu>
2020-02-01 16:54:20 -0500

    Fixed variable names, made naming conventions relevant to current servos

--- beaglebone/sa/src/__main__.py
+++ beaglebone/sa/src/__main__.py
@@ -2,21 +2,22 @@ import asyncio
 import time
 import Adafruit_BBIO.PWM as PWM
 from rover_common import aiolcm
-from rover_msgs import Servo
+from rover_msgs import ServoCmd
 
 SERVO_MAX_DC = 10.0
 SERVO_MIN_DC = 4.0
 
 #Blue Science Box
-SERVO_AMMONIA_1 = "P9_14"
-SERVO_AMMONIA_2 = "P9_16"
+SERVO_BLUE_0 = "P9_14"
+SERVO_BLUE_1 = "P9_16"
 #Yellow Science Box
-SERVO_AMMONIA_3 = "P9_21"
-SERVO_AMMONIA_4 = "P9_22"
+SERVO_YELLOW_0 = "P9_21"
+SERVO_YELLOW_1 = "P9_22"
 #White Science Box
-SERVO_AMMONIA_5 = "P8_13"
-SERVO_AMMONIA_6 = "P8_19"
-servos = [SERVO_AMMONIA_1, SERVO_AMMONIA_2, SERVO_AMMONIA_3, SERVO_AMMONIA_4, SERVO_AMMONIA_5, SERVO_AMMONIA_6]
+SERVO_WHITE_0 = "P8_13"
+SERVO_WHITE_1 = "P8_19"
+
+servos = [SERVO_BLUE_0, SERVO_BLUE_1, SERVO_YELLOW_0, SERVO_YELLOW_1, SERVO_WHITE_0, SERVO_WHITE_1]
 
 lcm_ = aiolcm.AsyncLCM()
 
@@ -36,17 +37,17 @@ def servo_init(pin, degrees):
 def servo_callback(channel, msg):
     servo = Servo.decode(msg)
     if servo.id == "servo_1":
-        run_servo(servos[0], servo.degrees)
+        run_servo(servos[0], servo.position)
     elif servo.id == "servo_2":
-        run_servo(servos[1], servo.degrees)
+        run_servo(servos[1], servo.position)
     elif servo.id == "servo_3":
-        run_servo(servos[2], servo.degrees)
+        run_servo(servos[2], servo.position)
     elif servo.id == "servo_4":
-        run_servo(servos[3], servo.degrees)
+        run_servo(servos[3], servo.position)
     elif servo.id == "servo_5":
-        run_servo(servos[4], servo.degrees)
+        run_servo(servos[4], servo.position)
     elif servo.id == "servo_6":
-        run_servo(servos[5], servo.degrees)
+        run_servo(servos[5], servo.position)
     else:
         print("Invalid servo ID.")
 
@@ -54,5 +55,5 @@ def main():
     for pin in servos:
         servo_init(pin, 0)
 
-    lcm_.subscribe("/servo", servo_callback)
+    lcm_.subscribe("/servo_cmd", servo_callback)
     
